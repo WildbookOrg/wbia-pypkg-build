286cd9bf9ef7381c23e5f695014ab42173dfeccd
Jason Parham <bluemellophone@gmail.com>
2020-07-07 20:17:34 -0700

    Lowercase

--- publish.github.packages.sh
+++ publish.github.packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker push docker.pkg.github.com/WildbookOrg/wbia-pypkg-build/manylinux:opencv-4.2.0
-docker push docker.pkg.github.com/WildbookOrg/wbia-pypkg-build/manylinux:opencv-3.4.10
-docker push docker.pkg.github.com/WildbookOrg/wbia-pypkg-build/manylinux:latest
+docker push docker.pkg.github.com/wildbookorg/wbia-pypkg-build/manylinux:opencv-4.2.0
+docker push docker.pkg.github.com/wildbookorg/wbia-pypkg-build/manylinux:opencv-3.4.10
+docker push docker.pkg.github.com/wildbookorg/wbia-pypkg-build/manylinux:latest
