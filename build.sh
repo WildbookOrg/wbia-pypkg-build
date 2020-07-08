b68bda6321723093a4bfda0f61304ee281bfdf9b
Jason Parham <bluemellophone@gmail.com>
2020-07-07 20:58:25 -0700

    Using smaller

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ docker build --build-arg OPENCV_VERSION="4.2.0" -t wildme/manylinux:opencv-4.2.0
 docker build --build-arg OPENCV_VERSION="3.4.10" -t wildme/manylinux:opencv-3.4.10 .
 docker tag wildme/manylinux:opencv-4.2.0 wildme/manylinux:latest
 
-docker tag wildme/manylinux:opencv-4.2.0 docker.pkg.github.com/wbia-pypkg-build/manylinux:opencv-4.2.0
-docker tag wildme/manylinux:opencv-3.4.10 docker.pkg.github.com/wbia-pypkg-build/manylinux:opencv-3.4.10
-docker tag wildme/manylinux:latest docker.pkg.github.com/wbia-pypkg-build/manylinux:latest
+docker tag wildme/manylinux:opencv-4.2.0 docker.pkg.github.com/wildbookorg/wbia-pypkg-build/manylinux:opencv-4.2.0
+docker tag wildme/manylinux:opencv-3.4.10 docker.pkg.github.com/wildbookorg/wbia-pypkg-build/manylinux:opencv-3.4.10
+docker tag wildme/manylinux:latest docker.pkg.github.com/wildbookorg/wbia-pypkg-build/manylinux:latest
